def360d14b6ad38bcb40ac1d44e9b918ddbf5b70
thahn0402 <19130207@st.hcmuaf.edu.vn>
2023-05-07 15:09:39 +0700

    Search

--- src/main/java/nlu/cdw23backend/service/ProductService.java
+++ src/main/java/nlu/cdw23backend/service/ProductService.java
@@ -20,9 +20,17 @@ public class ProductService {
         return productDao.save(product);
     }
 
-    public List<Product> getAllProducts(int pageNumber) {
-        Pageable pageable = PageRequest.of(pageNumber, 2);
-        return (List<Product>) productDao.findAll(pageable);
+    public List<Product> getAllProducts(int pageNumber, String searchKey) {
+        Pageable pageable = PageRequest.of(pageNumber, 12);
+
+        if (searchKey.equals("")) {
+            return (List<Product>) productDao.findAll(pageable);
+        } else {
+            return (List<Product>) productDao.findByProductNameContainingIgnoreCaseOrProductDescriptionContainingIgnoreCase(
+                    searchKey, searchKey, pageable
+            );
+        }
+
     }
 
     public void deleteProductDetails(Integer productId) {
