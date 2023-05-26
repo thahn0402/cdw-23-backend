0aeb38e820109dbfd39ed531f16dbeaa75115213
PhamDinhQuan <19130183@st.hcmuaf.edu.vn>
2023-05-26 23:12:20 +0700

    no message

--- src/main/java/nlu/cdw23backend/entity/Product.java
+++ src/main/java/nlu/cdw23backend/entity/Product.java
@@ -8,31 +8,41 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+
     private Integer productId;
     private String productName;
     @Column(length = 2000)
+
     private String productDescription;
     private Double productDiscountedPrice;
     private Double productActualPrice;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+
     @JoinTable(name = "product_images",
+
             joinColumns = {
                     @JoinColumn(name = "product_id")
+
+
             },
             inverseJoinColumns = {
                     @JoinColumn(name = "image_id")
+
             }
     )
     private Set<ImageModel> productImages;
+
 
     public Set<ImageModel> getProductImages() {
         return productImages;
     }
 
+
     public void setProductImages(Set<ImageModel> productImages) {
         this.productImages = productImages;
     }
+
 
     public Integer getProductId() {
         return productId;
